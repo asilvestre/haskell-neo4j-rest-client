e6251e97b61cb608b61d927b3b0f5104ec0f73a9
Antoni Silvestre <antoni.silvestre@gmail.com>
2014-07-22 00:24:32 +0200

    Added labels to batch and tests

--- src/Database/Neo4j/Batch/Label.hs
+++ src/Database/Neo4j/Batch/Label.hs
@@ -3,6 +3,7 @@
 
 module Database.Neo4j.Batch.Label where
 
+import Data.Maybe (fromMaybe)
 import Data.String (fromString)
 import Network.HTTP.Base (urlEncodeVars)
 
@@ -18,6 +19,8 @@ import Database.Neo4j.Types
 import Database.Neo4j.Batch.Node
 import Database.Neo4j.Batch.Types
 
+parseLabelsPath :: J.Value -> T.Text -> NodeUrl
+parseLabelsPath j suf = NodeUrl $ let p = tryParseFrom j in fromMaybe p $ T.stripSuffix suf p
 
 -- | Retrieve all labels for a node, if the node doesn't exist already it will raise an exception
 -- | Raises Neo4jNoEntityException if the node doesn't exist
@@ -25,7 +28,7 @@ getLabels :: NodeBatchIdentifier a => a -> Batch (BatchFuture [Label])
 getLabels n = nextState cmd
     where cmd = defCmd{cmdMethod = HT.methodGet, cmdPath = path, cmdBody = "", cmdParse = parser}
           path = getNodeBatchId n <> "/labels"
-          parser jn = G.setNodeLabels (NodeUrl $ tryParseFrom jn) (tryParseBody jn)
+          parser jn = G.setNodeLabels (parseLabelsPath jn "/labels") (tryParseBody jn)
 
 -- | Get all nodes using a label and a property
 getNodesByLabelAndProperty :: Label -> Maybe (T.Text, PropertyValue) -> Batch (BatchFuture [Node])
@@ -41,22 +44,23 @@ getNodesByLabelAndProperty lbl prop = nextState cmd
 -- | Raises Neo4jNoEntityException if the node doesn't exist
 addLabels :: NodeBatchIdentifier a => [Label] -> a -> Batch (BatchFuture ())
 addLabels lbls n = nextState cmd
-    where cmd = defCmd{cmdMethod = HT.methodPost, cmdPath = path, cmdBody = "", cmdParse = parser}
+    where cmd = defCmd{cmdMethod = HT.methodPost, cmdPath = path, cmdBody = J.toJSON lbls, cmdParse = parser}
           path = getNodeBatchId n <> "/labels"
-          parser jn g = let from = (NodeUrl $ tryParseFrom jn) in foldl (flip (G.addNodeLabel from)) g lbls
+          parser jn g = let from = parseLabelsPath jn "/labels" in foldl (flip (G.addNodeLabel from)) g lbls
 
 -- | Change node labels
 -- | Raises Neo4jNoEntityException if the node doesn't exist
 changeLabels :: NodeBatchIdentifier a => [Label] -> a -> Batch (BatchFuture ())
 changeLabels lbls n = nextState cmd
-    where cmd = defCmd{cmdMethod = HT.methodPut, cmdPath = path, cmdBody = "", cmdParse = parser}
+    where cmd = defCmd{cmdMethod = HT.methodPut, cmdPath = path, cmdBody = J.toJSON lbls, cmdParse = parser}
           path = getNodeBatchId n <> "/labels"
-          parser jn = G.setNodeLabels (NodeUrl $ tryParseFrom jn) lbls
+          parser jn = G.setNodeLabels (parseLabelsPath jn "/labels") lbls
 
 -- | Remove a label for a node
 -- | Raises Neo4jNoEntityException if the node doesn't exist
 removeLabel :: NodeBatchIdentifier a => Label -> a -> Batch (BatchFuture ())
 removeLabel lbl n = nextState cmd
-    where cmd = defCmd{cmdMethod = HT.methodPut, cmdPath = path, cmdBody = "", cmdParse = parser}
-          path = getNodeBatchId n <> "/labels/" <> lbl
-          parser jn = G.deleteNodeLabel (NodeUrl $ tryParseFrom jn) lbl
+    where cmd = defCmd{cmdMethod = HT.methodDelete, cmdPath = path, cmdBody = "", cmdParse = parser}
+          suffix = "/labels/" <> lbl
+          path = getNodeBatchId n <> suffix
+          parser jn = G.deleteNodeLabel (parseLabelsPath jn suffix) lbl
