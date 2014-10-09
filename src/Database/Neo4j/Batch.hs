2bcad2554084dcec4e08e540f1a36be36cc3bbf8
Antoni Silvestre <antoni.silvestre@gmail.com>
2014-10-10 00:43:15 +0200

    Exporting more Batch types

--- src/Database/Neo4j/Batch.hs
+++ src/Database/Neo4j/Batch.hs
@@ -6,7 +6,7 @@ module Database.Neo4j.Batch (
     -- $use    
 
     -- * General
-    Batch, runBatch, BatchFuture(..),
+    Batch, runBatch, BatchFuture(..), NodeBatchIdentifier, RelBatchIdentifier,
     -- * Nodes
     createNode, getNode, deleteNode,
     -- * Relationships
